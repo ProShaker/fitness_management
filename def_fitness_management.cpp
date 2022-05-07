705440a0212755a908a36c23c293d1aff0b08b00
KimHyeongJun <dktk500@naver.com>
2022-05-07 20:56:09 +0900

    초기 커밋.

---
+++ def_fitness_management.cpp
@@ -0,0 +1,34 @@
+#include "def_fitness_management.h"
+
+void normal_management::add_member()
+{
+}
+
+void normal_management::rewrite_member()
+{
+}
+
+void normal_management::rewrite_member()
+{
+}
+
+void normal_management::search_member()
+{
+}
+
+void normal_management::print_member()
+{
+}
+
+void normal_management::delete_member()
+{
+}
+
+int normal_management::reTurn()
+{
+	return 0;
+}
+
+void action_fitness_management()
+{
+}
---
+++ def_fitness_management.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+const int name_len = 11;
+
+void action_fitness_management();
+
+//일반 회원 클래스
+class normal_management {
+
+//개인정보는 private으로 보호한다.
+private:
+	char normal_name[name_len];
+	int normal_hpnumber;
+	char normal_gender;
+
+public:
+//운동 시작일, 운동복 관리 변수는 공용으로 값을 다루기 편하게 한다.
+	const int normal_start;
+	int normal_end;
+	bool normal_cloths;
+	const int normal_locker;
+
+	void add_member();
+	void rewrite_member();
+	void search_member();
+	void print_member();
+	void delete_member();
+	int reTurn();
+};
+
+// 특별 클래스 - 노말 클래스의 상속
+class special_management : normal_management
+{
+public:
+	char class_time;
+	char class_day[2];
+	char trainer[name_len];
+	const int PT_number;
+};
+
+// 스피닝 클래스 - 특별 클래스의 상속
+class spinning_management : special_management
+{
+};
+
+// PT 클래스 - 특별 클래스의 상속
+class pt_management : special_management
+{
+};
+
+// 필라테스 클래스 - 특별 클래스의 상속
+class pilates_management : special_management
+{
+};
+
+
